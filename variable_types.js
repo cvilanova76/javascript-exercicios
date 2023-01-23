724c2f74fa8f104a602d00b944f18ffc7235616c
cvilanova76 <carolinavilanova@gmail.com>
2023-01-23 17:57:37 -0300

    variable types incomplete

---
+++ variable_types.js
@@ -0,0 +1,19 @@
+var name = "carolina";
+var name2 = 'luiz';
+var name3 = `luciana`;
+
+let number = 46;
+let decimalNumber = 46.9;
+
+let fruits = ['apple', 'grape','watermelon'];
+
+let megaSena = [10, 20, 3, 40, 5, 60];
+let lista = ['teste', 12, true, 15.5];
+
+
+typeof name 
+typeof number
+typeof decimalNumber 
+typeof fruits
+typeof megaSena
+typeof lista
