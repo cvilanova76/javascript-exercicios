9e0f50c9822df84e09d0d4345d0d5ef99b826da8
cvilanova76 <carolinavilanova@gmail.com>
2023-01-24 17:22:13 -0300

    operadores incompleto

---
+++ operators.js
@@ -0,0 +1,33 @@
+var number = 20;
+
+number += 10;
+number -= 10.5;
+number *= 5;
+number /20;
+
+console.log(number)
+
+// retorna o resto da divisão
+const result = number % 5
+
+console.log(result)
+
+
+let number = 20;
+
+const maiorQue = number > 20; // false
+const maiorIgual = number >=20; // true
+const menorQue = number <20; // false
+const menorIgual = number <=20; // true
+const igual = number ==20; // true
+const diferente = number !=20; //false
+
+
+
+// compara valor e tipo
+const estritamenteIgual = number === "20";
+const estritamenteDiferente = number === 20;
+
+igual // igual
+estritamenteIgual // false
+
