96480d10b6ed60c3c9e8c27e106e93437f634d1b
cvilanova76 <carolinavilanova@gmail.com>
2023-01-24 16:49:44 -0300

    tipos de variáveis finalizados

--- variable_types.js
+++ variable_types.js
@@ -10,6 +10,48 @@ let fruits = ['apple', 'grape','watermelon'];
 let megaSena = [10, 20, 3, 40, 5, 60];
 let lista = ['teste', 12, true, 15.5];
 
+let house = {
+    "address": "Rua x",
+    "toilet": 2,
+    color: "branca",
+    rented: true
+  };
+  
+  
+  let professionalExperience = {
+    position: "Repórter",
+    company: "Folha de S.Paulo",
+    date_begin: "07/2021",
+    date_end: null,
+    description: "lorem ipsum dolor sit",
+    activities: ["atividade 1", "atividade 2"]
+    };
+  
+  let experiences = [
+    {
+    position: "Repórter",
+    company: "Folha de S.Paulo",
+    date_begin: "07/2021",
+    date_end: null,
+    description: "lorem ipsum dolor sit",
+    activities: ["atividade 1", "atividade 2"]
+    }, 
+    {
+    position: "Jornalista",
+    company: "Autônomo",
+    date_begin: "09/2019",
+    date_end: "07/2021",
+    description: "lorem ipsum dolor sit",
+    activities: ["atividade 1", "atividade 2"]
+    }
+  ];
+  
+  experiences[0].position
+  
+  experiences[1].position
+  
+  
+
 
 typeof name 
 typeof number
@@ -17,3 +59,8 @@ typeof decimalNumber
 typeof fruits
 typeof megaSena
 typeof lista
+typeof house;
+typeof house.address;
+typeof house.rented;
+typeof experiences;
+typeof professionalExperience;
