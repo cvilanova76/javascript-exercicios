47f74b60773aa53689cf39a5bc3e2b40e8a5eb48
cvilanova76 <carolinavilanova@gmail.com>
2023-01-26 19:01:13 -0300

    repeticoes incompletas

--- repetitions.js
+++ repetitions.js
@@ -18,9 +18,40 @@ for (let contador = 0; contador <= 10 ; contador ++){
 // conta de 0 a 10  
 
 
-//  loop infinito -- dá mensagem de erro
+for (let contador = 10; contador > 0 ; contador --){
+    console.log(contador)
+  };
+// contagem regressiva de 10 a 1
+
+for (let contador = 10; contador >= 0 ; contador --){
+    console.log(contador)
+  };
+// contagem regressiva de 10 a 0  
+  
+
 for(let contador = 10; contador <= 10; contador--){
     console.log("contador" + contador)
   };
-  
+  //  loop infinito -- dá mensagem de erro  
+
+  let number = 42;
+let dividor = 1;
+
+while (dividor <= number) {
+  let rest = number % dividor;
+  if (rest == 0) {
+    console.log("Dividor found: " + dividor);
+  }
+  dividor = dividor + 1;
+}
+// dá o resultado do resto da divisao
+
+let counter = 0;
+
+do {
+  console.log("Counter is valid: " + counter);
+} while (counter == 1);
+// 
+
+
   
